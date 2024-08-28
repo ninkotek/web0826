d7749c470b8e5ad9b03ccb9352245983794a810c
ninkotek <yuskang@hotmail.com>
2024-08-28 13:59:42 +0900

    0828

--- cra/ui-card/src/App.jsx
+++ cra/ui-card/src/App.jsx
@@ -2,11 +2,9 @@ import Card from "./Card.jsx";
 
 function App() {
   return (
-    <div style={{display: "flex", padding: "4px"}}>
+    <>
       <Card />
-      <Card />
-      <Card />
-    </div>
+    </>
   );
 }
 
---
+++ cra/ui-card/src/Card.jsx
@@ -0,0 +1,17 @@
+import pic01 from "./assets/pic01.jpg";
+
+function Card() {
+  return (
+    <div className="card">
+      <h3>BORA BORA</h3>
+      <img src={pic01} alt="" />
+      <p>
+        Lose yourself in a private island paradise, where the waters teem with
+        exotic fish and colorful coral.
+      </p>
+      <span>VIEW PROPERTY</span>
+    </div>
+  );
+}
+
+export default Card;
